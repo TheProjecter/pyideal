9b56802e72413d50ae5ceb6b21d1800709365e78
Diego Sarmentero <diego.sarmentero@gmail.com>
2010-09-21 21:59:14 -0300

    changes in project structure

---
+++ plugins/pluginbase.py
@@ -0,0 +1,11 @@
+class PluginBase(object):
+    _name = 'Unnamed Plugin'
+    _description = "No description"
+    _icon = ''
+
+    _authors = []
+    _active = False
+
+    def __init__(self):
+        """Set the Plugin configuration"""
+        pass
